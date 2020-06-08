49cb912f5c8fc948f886190a2aa07b4a991e6119
Siham Hachi <siham.hachi@uni.lu>
2020-06-08 12:12:08 -0400

    first test on heroku

--- app.py
+++ app.py
@@ -1,88 +1,44 @@
-'''
-Name 0.0
-
-Web Application for Insight Health Data Science Fellowship,
-Boston, MA -- 2020A
-
-Author: Siham Hachi
-
-Last Revision: 2020.06.03
-
-'''
-
-
 import sys
 sys.path.insert(1, './src')
+sys.path.insert(1, './models')
+import os
 
-import streamlit as st
+from flask import Flask, render_template, request
+
 import librosa
 import tools as tl
-import csv
-import pandas as pd
-import numpy as np
-import os
-import io
 import prediction
-from pyprojroot import here
-import streamlit_theme as stt
 
-import shutil
+from werkzeug import secure_filename
+app = Flask(__name__)
 
-from typing import Dict
+@app.route('/')
+def home():
+    return render_template('index.html')
 
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')    
 
-#st.markdown('<style>' + open('style.css').read() + '</style>', unsafe_allow_html=True)
-#st.markdown("<h1 style='text-align: center;' class='test'>Welcome to SpeaKid!</h1>", unsafe_allow_html=True)
+@app.route('/results', methods = ['GET', 'POST'])
+def results():
 
-stt.set_theme({'primary': '#63d297'})
-st.title("Welcome to Thrive!")
-st.markdown(
-"""
-This is a demo of a Streamlit app that recognises children with speech pathology 
-based on morpheme recordings. Upload an audio file below.
-""")
-
-@st.cache(allow_output_mutation=True)
-def get_static_store() -> Dict:
-    """This dictionary is initialized once and can be used to store the files uploaded"""
-    return {}
-
-def main():
-    """Run this function to run the app"""
-    static_store = get_static_store()
-
-    input_buffer = st.file_uploader("Upload", type="wav")
-    if input_buffer:
-        # Process you file here
-        value = input_buffer.getvalue()
-
-        # And add it to the static_store if not already in
-        if not value in static_store.values():
-            static_store[input_buffer] = value
-
-        filename = 'testSave.wav'
-        input_buffer.seek(0)
-        with open(filename, 'wb') as f:
-            shutil.copyfileobj(input_buffer, f)
-
-        y, sr = librosa.load(filename, mono=True, duration=30)
-
-        features = tl.load_audio_data([filename], savecsv=0, csvname='')
-        modelpath = here() / 'models/DLmodel.h5'
+    f = request.files['audio']
+    if f.filename != '':
+        f.save(secure_filename(f.filename))
+        y, sr = librosa.load(f.filename, mono=True, duration=30)
+        features = tl.load_audio_data([f.filename], savecsv=0, csvname='')
+        modelpath = './models/DLmodel.h5'
         p = prediction.predict_NN(features, modelpath)
 
         if p == 0:
-            st.title('There seems to be speech disorder')
-        else: st.title('No speech disorder has been detected')
+            return render_template('upload.html', upload_text='There seems to be speech disorder')
+        else:
+            return render_template('upload.html', upload_text='No speech disorder has been detected')
+        os.remove()
+    else:
+        return render_template('upload.html', upload_text='Please upload a file') 
+        
 
-        os.remove(filename)
-
-
-main()            
-
-
-
-    
-
-
-
+if __name__ == '__main__':
+   app.run(debug = True)
